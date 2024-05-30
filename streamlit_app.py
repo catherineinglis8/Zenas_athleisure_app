96f16c27b78f2db1977951fdd8b520898e1a405c
catherineinglis8 <catherine.inglis-groulx@larochellegc.com>
2024-05-30 08:25:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**st.secrets["connections.snowflake"])
+#my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(
+user = "catherineinglis"
+password = "CiGdr1*6"
+account = "QS89075.ca-central.aws"
+warehouse = "compute_wh"
+database = "ZENAS_ATHLEISURE_DB"
+schema = "PRODUCTS")
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
+
+
