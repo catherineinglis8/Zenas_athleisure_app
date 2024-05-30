53693c923349a2a3e68f647c0761e93731f47446
catherineinglis8 <catherine.inglis-groulx@larochellegc.com>
2024-05-30 09:23:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,46 +14,23 @@ my_catalog = my_cur.fetchall()
 
 #Put the data into a panda dataframe
 df = pd.DataFrame(my_catalog)
+color_list = df[0].values.tolist()
 
 #st.write(df)
 #st.stop()
-st.selectbox("Pick a sweatsuit color of style:",df)
-
+color_selected = st.selectbox("Pick a sweatsuit color or style:",list(color_list))
 st.stop()
-#display photo
+caption = 'Our warm, comfortable, ' + color_selected + 'sweatsuit!'
 
+my_cur.execute("SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+#display photo with caption
+st.image(df[0],widght = 400,caption = product_caption)
 #display writing
 
 
 
-
-
-
-ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections = 5)
-if ingredients_list:
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-   # st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+ name_on_order +"""')"""
-
-    
-    #st.write(my_insert_stmt)
-    # if ingredients_string:
-    time_to_insert = st.button("Submit Order")
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
 
 
 
